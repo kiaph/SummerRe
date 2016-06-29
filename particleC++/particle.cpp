6f107bebdde6b0b575dce4b2784eec471e8ac1cc
kiaph <heiro@gmx.com>
2016-06-28 22:00:19 -0400

    cant debug git status woops q "" " git git quit damn / \\ q Q1 QUIT exit ./edit git add * git status git commit "broke it" " " "

--- particleC++/particle.cpp
+++ particleC++/particle.cpp
@@ -4,6 +4,8 @@
 double myrand();
 using namespace std;
 
+
+
 class Particle{
 
     public:
@@ -16,8 +18,24 @@ class Particle{
             this->x = x;
             this->y = y;
             this->vx = vx;
-            this->vy = vy;         
+            this->vy = vy;    
+            double particleX(double);
+            double particleVX(double);
+            
         }
+        
+            double particleX(double x = 0)
+            {
+            //this->x = x;
+            x = this->x;
+            return x;
+            }
+            double particleVX(double vx = 0)
+            {
+            //this->vx = vx;
+            vx = this->vx;
+            return vx;
+            }
     private:
         
         double x;
@@ -30,12 +48,15 @@ class Particle{
 };
 
 
+
+
 class simBox{
     
     public:
         
         //setting up constructor 
-        simBox(double X0 = 0,double XR = 0,double Y0 = 0,double YT = 0){
+        simBox(double X0 = 0,double XR = 0,double Y0 = 0,double YT = 0)
+        {
         
             this->X0=X0;
             this->XR=XR;
@@ -47,14 +68,19 @@ class simBox{
             
         }
         
-        double bCondition(double p)
+        /*double bCondition(double p)
         {
-            using namespace std;
-            cout >> "Testing , uh bC?" >> endl;
+            
+            //cout >> "Testing , uh bC?" >> endl;
+            double Particle::particleX();
+            double Particle::particleVX();
             double x0 = this->X0;
             double xR = this->XR;
             double y0 = this->Y0;
             double yT = this->YT;
+            double x = Particle::particleX();
+            double vx = Particle::particleVX();
+            
             // add accumulators to calculate pressure at boundaries
             if(p.x < x0)
             {
@@ -62,10 +88,44 @@ class simBox{
                 double p.vx = -p.vx;
                 
                 
+            }*/
+            
+            
+        /*def make_particle(self):
+        #print("make p")
+        x = self.X0 + (self.XR-self.X0)*random()
+        y = self.Y0 + (self.YT-self.Y0)*random()
+        vx = 2*( 0.5 - random() )
+        vy = 2*( 0.5 - random() )
+        return Particle(x,y,vx,vy)*/
+        
+            Particle make_particle()
+            {
+                double x = this->X0 + (this->XR - this->X0)*myrand();
+                double y = this->Y0 + (this->YT - this->Y0)*myrand();
+                double vx = 2*(0.5 - myrand() );
+                double vy = 2*(0.5 - myrand() );
+                Particle(x,y,vx,vy);
+                //#include <iostream>
+                //#include <stdlib.h>article ;simBox::make_particle;
+                
+
+                char printthis();
+                printthis();
+                //cout >> "testing" ;
+                return Particle(x,y,vx,vy);
+                
+                //return x,y,vx,vy;
+            }
+        
+            void printthis()
+            {
+                //char a = "printthis" ;
+            
+                cout << "a" << endl;
             }
             
             
-        }
         
         
     private:
@@ -81,10 +141,19 @@ class simBox{
     
     
 };
+//static simBox(0,0,0,0);
+//Particle simBox::make_particle(0,0,0,0);
+simBox simBox();
+
 
 int main(){
 	cout << myrand() << endl; 
 	cout << myrand() << endl;
+        Particle Particle();
+        void printthis();
+        Particle; simBox; 
+        
+        
 	return 0;
         
 	
